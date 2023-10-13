d463567fa1bc7bceca946fe4748bf4f44591069f
PawanSekhon <133436526+PawanSekhon@users.noreply.github.com>
2023-10-13 12:12:43 -0400

    Create data folder and inside it i added BloomiBlanketContext.cs and updated it.

---
+++ Data/BloomiBlanketContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BloomiBlanket.Models;
+
+namespace BloomiBlanket.Data
+{
+    public class BloomiBlanketContext : DbContext
+    {
+        public BloomiBlanketContext(DbContextOptions<BloomiBlanketContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blanket> Blanket { get; set; }
+    }
+}
