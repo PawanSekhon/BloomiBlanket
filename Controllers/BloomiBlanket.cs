db87ccbde3c6b90e93d987e547dddaeff6339df2
PawanSekhon <133436526+PawanSekhon@users.noreply.github.com>
2023-10-13 11:50:35 -0400

    edited  and added index, AboutUs and welcome page and tested it

--- Controllers/BloomiBlanket.cs
+++ Controllers/BloomiBlanket.cs
@@ -1,8 +1,6 @@
-﻿ using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
 
 namespace BloomiBlanket.Controllers
 {
@@ -10,6 +8,14 @@ namespace BloomiBlanket.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
             return View();
         }
     }
