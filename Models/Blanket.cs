ddef2ff2c6489d4e7e2b88052d057ade7ec3a698
PawanSekhon <133436526+PawanSekhon@users.noreply.github.com>
2023-10-13 12:03:21 -0400

    Created Blanket.cs file in Models and updated its class.

---
+++ Models/Blanket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloomiBlanket.Models
+{
+    public class Blanket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ManufacturingDate { get; set; }
+        public string Material { get; set; }
+        public string Size { get; set; }
+        public string Colour { get; set; }
+        public decimal Price { get; set; }
+        public int CustomerRating { get; set; }
+    }
+}
